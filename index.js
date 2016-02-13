caa9f153aa3c7422d329198634902dab2f3993d0
Ivshti <ivo@strem.io>
2016-02-14 01:29:13 +0200

    correct retrieval of cuepoints for mkv

--- index.js
+++ index.js
@@ -13,11 +13,32 @@ function onlySeekCues() {
 	};
 }
 
-
+function atPath() {
+	var args = Array.prototype.slice.call(arguments)
+	var arg
+	var data = args.shift()
+	while (arg = args.shift()) {
+		if (! arg) return data;
+		if (! data.children) return;
+		data = data.children.filter(function(x) { return x._name === arg })[0]
+		if (! data) return
+	}
+	return data
+}
 function getForMkv(url, cb) {
 	var decoder = new mkv.Decoder(onlySeekCues());
 	decoder.parseEbmlIDs(url, [ mkv.Schema.byName.Cues ], function(err, doc) {
-		cb(err, doc)
+		var cues = atPath(doc, "Segment", "Cues");
+		if (! (cues && cues.children && cues.children.length)) return cb(new Error("no cues found in doc -> Segment -> Cues"));
+
+		cues = cues.children.filter(function(x) { return x._name === "CuePoint" }) 
+
+		if (! cues.length) return cb(new Error("no CuePoints"));
+
+		cb(null, cues.map(function(cue) {
+			// children[0] is CueTime
+			return cue.children[0].getUInt()
+		}))
 		// "doc -> Segment -> Cues -> CuePoint []"
 	})
 }
