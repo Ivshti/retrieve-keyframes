13bbf0ba292627015b44d1f037acc0d2d9aba809
Ivo Georgiev <ivo@linvo.org>
2016-07-13 14:45:06 +0300

    cli: now can log frame seconds

--- cli.js
+++ cli.js
@@ -5,6 +5,8 @@ var retrieveKeyframes = require('./')
 var file = process.argv[2]
 var fn = file.match('.mp4$') ? retrieveKeyframes.getForMp4 : (file.match('.mkv$') ? retrieveKeyframes.getForMkv : null)
 
+var isSeconds = process.argv.some(function(arg) { return arg === '--seconds' })
+
 if (! fn) {
 	console.error('pass a mkv or mp4 file')
 	process.exit(1)
@@ -13,5 +15,6 @@ if (! fn) {
 fn(file, function(err, res) {
 	if (err) return console.error(err)
 
-	res.forEach(function(frame) { console.log(frame) })
+	if (isSeconds) res.forEach(function(frame) { console.log((frame.timestamp / 1000).toFixed(6)) })
+	else res.forEach(function(frame) { console.log(frame) })
 })
