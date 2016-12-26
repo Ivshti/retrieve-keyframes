f2a7ad3238cc69b98539f3a84365db955984c47b
Ivo Georgiev <ivo@linvo.org>
2016-12-26 18:20:08 +0200

    keyframes.sh: add media_type to printed vals, in case we want other frames too

--- keyframes.sh
+++ keyframes.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-ffprobe -select_streams v -show_frames -show_entries frame=pkt_dts_time,pkt_pts_time,key_frame -of csv "$1" | grep -n --color=auto frame,1
+ffprobe -select_streams v -show_frames -show_entries frame=media_type,pkt_dts_time,pkt_pts_time,key_frame -of csv "$1" | grep -n --color=auto frame,1
 
